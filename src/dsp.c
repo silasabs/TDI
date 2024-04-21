88d1746a9fcb2f01329f8e7185f36e75310841f0
Silas João <silasroot321@gmail.com>
2024-04-21 16:15:01 -0300

    Implementa a função de normalização.

--- src/dsp.c
+++ src/dsp.c
@@ -2,10 +2,39 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <complex.h>
 
 /*
-  Implementa a geração de uma sequência de bits pseudo-aleatórios 
-  que chegam ao transmissor.
+    Normaliza a potência média de cada componente de nums.
+Input: 
+    nums (float complex): fasores da constelação.
+    size (int): comprimento do sinal
+Output: 
+    normalized_nums (float complex): Sinal com cada componente normalizado em potência.
+*/
+
+float complex *pnorm(float complex* nums, int size) {
+    // parameters
+    float complex sum = 0;
+    float complex norm;
+    // normalized constellation
+    float complex *normalized_nums = (float complex*)malloc(size * sizeof(float complex));
+    // calculate the average of the phasors
+    for(int i = 0; i < size; i++) {
+        sum += nums[i]*conj(nums[i]);
+    }
+    // normalization parameter
+    norm = sqrt(creal(sum/size));
+    // normalizes the QAM constellation
+    for(int i = 0; i < size; i++) {
+        normalized_nums[i] = nums[i]/norm;
+    }
+    return normalized_nums;
+}
+
+/*
+    Implementa a geração de uma sequência de bits pseudo-aleatórios 
+    que chegam ao transmissor.
 Input: 
     M (int): Ordem do esquema de modulação.
     Nbits (int): comprimento do bitstream
