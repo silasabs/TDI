51bf28d7e3dadfa4be6b5952a83e962529b6a19a
Silas João <silasroot321@gmail.com>
2024-05-09 17:54:53 -0300

    Update dsp.c

--- src/dsp.c
+++ src/dsp.c
@@ -12,6 +12,26 @@ const float complex QAM4_symbols[4] = {
    -1.0 - 1.0*I  // 10
 };
 
+float complex *qam4Mapper(int *bits, int length) {
+    float complex *symbols = (float complex*)malloc(length * sizeof(float complex));
+    int i, symbol_index = 0;
+    for (i = 0; i < length; i += 2) {
+        
+        int bit1 = bits[i];
+        int bit2 = bits[i + 1];
+
+        if (bit1 == 0 && bit2 == 0) {
+            symbols[symbol_index++] = QAM4_symbols[2]; // 00
+        } else if (bit1 == 0 && bit2 == 1) {
+            symbols[symbol_index++] = QAM4_symbols[0]; // 01
+        } else if (bit1 == 1 && bit2 == 0) {
+            symbols[symbol_index++] = QAM4_symbols[3]; // 10
+        } else if (bit1 == 1 && bit2 == 1) {
+            symbols[symbol_index++] = QAM4_symbols[1]; // 11
+        }
+    }
+    return symbols;
+}
 
 float **complex2float(float complex *arr, int length){
     // Aloca espaço para a matriz de partes reais e imaginárias
@@ -64,17 +84,11 @@ float complex *upsample(float complex* signal, int length, int factor) {
     for(int i = 0; i < length; i++) {
         signalUp[i*factor] = signal[i];
         for(int j = 1; j < factor; j++) {
-            signalUp[i*factor+j] = 0;
+            signalUp[i*factor+j] = 0.0 + 0.0*I;
         }
     }
     return signalUp;
 }
-
-// complex float* qam4Mapper(int *bits, int length, int M) {
-    
-//     float complex *constl = (float complex*)malloc(length/log2(M) * sizeof(float complex));
-
-// }
 
 /*
     Normaliza a potência média de cada componente de nums.
@@ -122,14 +136,43 @@ int *getRandomBits(int Nbits) {
 }
 
 int main(){
+    // Vetor de bits de exemplo
+    int Nbits = 16;
+    int SpS   = 16;
+    // Obtendo os bits aleatórios
+    int *Bits = getRandomBits(Nbits);
+    
+    for (int i = 0; i < Nbits; i++) {
+        printf("%d", Bits[i]);
+    }
+    printf("\n");
 
-    // gera bits de forma aleatória.
-    int M = 4;
-    //int Nbits = 1000*log2(M);
+    // Mapeia os bits para os símbolos da constelação 4QAM
+    float complex *symbTx = qam4Mapper(Bits, Nbits);
 
-    int *bits = getRandomBits(4);
-    //float complex *result = qam4Mapper(bits, 4, M);
-    printf("%f", cimag(QAM4_symbols[0]));
+    // Imprime os símbolos mapeados
+    printf("Símbolos mapeados para a constelação 4QAM:\n");
+    for (int i = 0; i < Nbits/2; i++) {
+       printf("%.1f + %.1fi\n", crealf(symbTx[i]), cimagf(symbTx[i]));
+    }
+    
+    // normaliza os simbolos mapeados para energia unitária
+    float complex *symbTxNorm = pnorm(symbTx, Nbits/2);
+
+    printf("Símbolos Normalizados:\n");
+    for (int i = 0; i < Nbits/2; i++) {
+       printf("%.4f + %.4fi\n", crealf(symbTxNorm[i]), cimagf(symbTxNorm[i]));
+    }
+
+    // Realiza o Upsampling
+    float complex *symbolsUp = upsample(symbTxNorm, Nbits/2, SpS);
+
+    printf("Após Upsampling:\n");
+    for (int i = 0; i < Nbits/2*SpS; i++) {
+       printf("%.4f + %.4fi\n", crealf(symbolsUp[i]), cimagf(symbolsUp[i]));
+    }
+
+    return 0;
 }
 
 // testa pnorm e arrays de 2D
